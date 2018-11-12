b4a86f0051a416e4a0c07bd7c48fc369230ae275
David Sn <divad.nnamtdeis@gmail.com>
2018-11-12 20:42:52 +0100

    Apply patches to local synced repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,53 @@
 #!/bin/bash
 
+ROOT_DIR=$(pwd)
+GIT_BASEURL="https://github.com/berkeley-pie"
+
 echo "Applying patches..."
+
+# android_system_sepolicy
+cd $ROOT_DIR/system/sepolicy
+git reset --hard HEAD
+git fetch $GIT_BASEURL/android_system_sepolicy
+git cherry-pick 5721f5ced4e600b4d632565cebea78fc8fdb4c46
+git cherry-pick 3bcedb6a927e1285305fca9118f549e4244483fc
+
+# android_frameworks_base 
+cd $ROOT_DIR/frameworks/base
+git reset --hard HEAD
+git fetch $GIT_BASEURL/android_frameworks_base
+git cherry-pick bbf3c8e2ba771d81a8f616bcce0ce314ca3b941b
+git cherry-pick 5996abee607c273118f3b9bc322051a635287ac7
+
+# android_frameworks_av 
+cd $ROOT_DIR/frameworks/av
+git reset --hard HEAD
+git fetch $GIT_BASEURL/android_frameworks_av
+git cherry-pick 5f1d2ff95839fdf527b4c7308e8cfc26d442b6b8
+
+# android_frameworks_native 
+cd $ROOT_DIR/frameworks/native 
+git reset --hard HEAD
+git fetch $GIT_BASEURL/android_frameworks_native
+git cherry-pick cab63cf62a7464185a2ef2ca5b7a5fbeb4a4faf5
+git cherry-pick 211613227afc4796dfc80ace060f7fe66ef97549
+git cherry-pick 54713f5c76d9cef734462e72f5e4724b4bcb57ec
+git cherry-pick 796958dc5f390e09bf18f7d10860e214982b899f
+git cherry-pick abd86859d2a06f919e82685f2a3e1eb59ca56614
+
+# android_frameworks_opt_telephony 
+cd $ROOT_DIR/frameworks/opt/telephony
+git reset --hard HEAD
+git fetch $GIT_BASEURL/android_frameworks_opt_telephony
+git cherry-pick 0b7bbebbb6946cfa8dc87cd8bdf0c4d4b5633cb7
+git cherry-pick bfb97a670ca79bc207dbe59bc25a393cae5de881
+git cherry-pick ea28fb53da79b9344d01b1b32018c2d442907bc5
+git cherry-pick 8f40da278354b2909686988bbd5ab062dc6dea1c
+
+# android_packages_services_Telephony
+cd $ROOT_DIR/packages/services/Telephony
+git reset --hard HEAD
+git fetch $GIT_BASEURL/android_packages_services_Telephony
+git cherry-pick 241038b61b65d2b7a531a8417786585063528056
+
+echo "Done!"
